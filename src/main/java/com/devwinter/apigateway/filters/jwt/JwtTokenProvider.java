bd8d4e98ee5174b112beebaeb3954a6c1cb12d7a
Lee Dong Hun <109887384+donghun93@users.noreply.github.com>
2023-02-13 09:24:43 +0900

    Feature/#6 jwt token valid filter add (#7)
    * Build: JWT 토큰 관련 라이브러리 추가
    
    * Docs: application-prod 필터 추가
    
    logout 요청 시 AuthorizationHeaderFilter 추가
    
    * Feat: JWT 토큰 기반 검증 필터 추가
    
    
    Resolvers: #6
    
    * Docs: JWT 토큰 암호화 키 추가
    
    
    Resolvers: #6

---
+++ src/main/java/com/devwinter/apigateway/filters/jwt/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.devwinter.apigateway.filters.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+    private final Key secretKey;
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret-key}") String secret
+    ) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Long getAudienceFromToken(String token) {
+        return Long.valueOf(getClaimFromToken(token, Claims::getAudience));
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                   .setSigningKey(secretKey)
+                   .build()
+                   .parseClaimsJws(token)
+                   .getBody();
+    }
+
+    public void tokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+    }
+}
